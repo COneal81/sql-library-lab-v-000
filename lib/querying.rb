4e658bc4ac749bfe3cbd6eb892aa5e718fe7144d
Kari O'Neal <triadfirewood@gmail.com>
2019-06-04 11:45:51 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -43,7 +43,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-   "SELECT characters.name,  
+   "SELECT characters.name, COUNT(character_books.book_id) AS book_num FROM character_books
+    INNER JOIN characters
+      ON
 end
 
 
